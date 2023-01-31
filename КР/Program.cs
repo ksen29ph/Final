cc4d6c2ddebe9ff43907c192558da2622241b7f6
ksen29ph <119218516+ksen29ph@users.noreply.github.com>
2023-01-31 21:52:26 +0300

    Add files via upload

---
+++ КР/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите строки массива через пробел");
+string line = Console.ReadLine();
+string[] array;
+array = line.Split(' ');
+var result =  new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize ++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
